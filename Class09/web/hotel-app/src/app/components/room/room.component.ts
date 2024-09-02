a234e80b0a3ea7616fc4d0e007680924d160b3e6
Venatik <stefan.trajkovski008@gmail.com>
2024-09-02 10:03:07 +0200

    folderfix

---
+++ Class09/web/hotel-app/src/app/components/room/room.component.ts
@@ -0,0 +1,44 @@
+import { Component, input, signal } from "@angular/core";
+import { Room } from "../../types/room.interface";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { CurrencyPipe } from "@angular/common";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule } from "@angular/material/badge";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TooltipPosition, MatTooltipModule } from "@angular/material/tooltip";
+import { CdkScrollable } from "@angular/cdk/scrolling";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+@Component({
+  selector: "app-room",
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CurrencyPipe,
+    MatExpansionModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkScrollable,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  templateUrl: "./room.component.html",
+  styleUrl: "./room.component.css",
+})
+export class RoomComponent {
+  room = input.required<Room>();
+  readonly panelOpenState = signal(false);
+
+  openPanel(isOpen: boolean) {
+    this.panelOpenState.set(isOpen);
+  }
+}
