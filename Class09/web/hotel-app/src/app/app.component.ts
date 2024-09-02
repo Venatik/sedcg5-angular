68bdd3092b7adbc4ea24d3cad1cdb43509cd4dd2
Venatik <stefan.trajkovski008@gmail.com>
2024-09-02 11:20:10 +0200

    class09

--- Class09/web/hotel-app/src/app/app.component.ts
+++ Class09/web/hotel-app/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { RxjsOperatorsComponent } from "./components/rxjs-operators/rxjs-operators.component";
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, RxjsOperatorsComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
--- Class09/web/hotel-app/src/app/app.routes.ts
+++ Class09/web/hotel-app/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
-import { RoomsComponent } from "./components/rooms/rooms.component";
+import { HomeComponent } from "./home/home.component";
+import { RoomsComponent } from "./rooms/rooms.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
